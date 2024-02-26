18d33484aa071a95a3dcf9f7929ff8074f02a876
Stephen Karanja <karanjastephenn@gmail.com>
2024-02-26 11:11:36 +0300

    simple web server

---
+++ express/intro/hello_node.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Using Node.js to create a simple web server using the Node HTTP package.
+//
+// Load HTTP module
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+// Create HTTP server
+const server = http.createServer(function (req, res) {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { "content-Type": "text/plain" });
+
+  // Send the response body "Hello World"
+  res.end("Hello World\n");
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, function () {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
