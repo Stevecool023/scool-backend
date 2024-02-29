680c0594104e73c35443665ec078d936d133b73a
Stephen Karanja <karanjastephenn@gmail.com>
2024-02-29 12:20:24 +0300

    genre Schema

---
+++ express/skeleton-website/express-locallibrary-website/models/genre.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 100
+    }
+  }
+);
+
+// Virtual for genre's URL
+GenreSchema.virtual('url').get(function () {
+  // Won't use arrow function inorder to deploy the this object
+  return `/catalog/genres/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Genre", GenreSchema);
