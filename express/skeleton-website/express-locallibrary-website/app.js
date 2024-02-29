0a12f62b0d90d31cb0527f86ba58c325dc07b9aa
Stephen Karanja <karanjastephenn@gmail.com>
2024-02-29 11:27:58 +0300

    mongodb connection

--- express/skeleton-website/express-locallibrary-website/app.js
+++ express/skeleton-website/express-locallibrary-website/app.js
@@ -9,6 +9,16 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://tallmankaranjastevey:ANTZH8Ao0CdP2CiH@cluster0.rewm74h.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
