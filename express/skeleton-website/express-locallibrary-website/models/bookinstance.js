bc7c2651b8716bd503918aa2a5b1d09b2f4754a0
Stephen Karanja <karanjastephenn@gmail.com>
2024-03-05 08:20:06 +0300

    formatted date

--- express/skeleton-website/express-locallibrary-website/models/bookinstance.js
+++ express/skeleton-website/express-locallibrary-website/models/bookinstance.js
@@ -16,13 +16,13 @@ const BookInstanceSchema = new Schema({
 });
 
 // virtual for bookinstance's URL
-BookInstanceSchema.virtual("url").get(function () {
+ BookInstanceSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/bookinstance/${this._id}`;
 });
 
 BookInstanceSchema.virtual("due_back_formatted").get(function () {
-  return DateTime.fromJSDate(this.due_back).toLocalString(DateTime.DATE_MED);
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model
